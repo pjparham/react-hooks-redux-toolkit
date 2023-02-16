386eeaba7db79bbaed079837d62a86ffdcf030bf
Parker Parham <parkerparham@gmail.com>
2023-02-16 17:33:04 -0500

    Done with assignment

--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,34 +1,42 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // Action Creators
 
 // async actions
-export function fetchCats() {
-  return function (dispatch) {
-    dispatch({ type: "cats/fetchCats/pending" });
-    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch({
-          type: "cats/fetchCats/fulfilled",
-          payload: data.images,
-        });
-      });
-  };
-}
+
+// export function fetchCats() {
+//   return function (dispatch) {
+//     dispatch({ type: "cats/fetchCats/pending" });
+//     fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         dispatch({
+//           type: "cats/fetchCats/fulfilled",
+//           payload: data.images,
+//         });
+//       });
+//   };
+// }
+export const fetchCats = createAsyncThunk("cats/fetchCats", () => {
+  // return a Promise containing the data we want
+  return fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+    .then((response) => response.json())
+    .then((data) => data.images);
+});
 
 // sync actions added for demo purposes
-export function catAdded(newCat) {
-  return {
-    type: "cats/catAdded",
-    payload: newCat,
-  };
-}
+// export function catAdded(newCat) {
+//   return {
+//     type: "cats/catAdded",
+//     payload: newCat,
+//   };
+// }
 
-export function catUpdated(updatedCat) {
-  return {
-    type: "cats/catUpdated",
-    payload: updatedCat,
-  };
-}
+// export function catUpdated(updatedCat) {
+//   return {
+//     type: "cats/catUpdated",
+//     payload: updatedCat,
+//   };
+// }
 
 // Reducer
 const initialState = {
@@ -36,38 +44,31 @@ const initialState = {
   status: "idle", // loading state
 };
 
-function catsReducer(state = initialState, action) {
-  switch (action.type) {
-    // sync actions
-    case "cats/catAdded":
-      return {
-        ...state,
-        entities: [...state.entities, action.payload],
-      };
-    case "cats/catUpdated":
-      return {
-        ...state,
-        entities: state.entities.map((cat) =>
-          cat.id === action.payload.id ? action.payload : cat
-        ),
-      };
+const catsSlice = createSlice({
+  name: "cats",
+  initialState,
+  reducers: {
+    catAdded(state, action) {
+      // using createSlice lets us mutate state!
+      state.entities.push(action.payload);
+    },
+    catUpdated(state, action) {
+      const cat = state.entities.find((cat) => cat.id === action.payload.id);
+      cat.url = action.payload.url;
+    },
+    // async actions to come...
+  },
+  extraReducers:{
+    [fetchCats.pending](state) {
+      state.status = "loading";
+    },
+    [fetchCats.fulfilled](state, action) {
+      state.entities = action.payload;
+      state.status = "idle";
+    },
+  },
+});
 
-    // async actions
-    case "cats/fetchCats/pending":
-      return {
-        ...state,
-        status: "loading",
-      };
-    case "cats/fetchCats/fulfilled":
-      return {
-        ...state,
-        entities: action.payload,
-        status: "idle",
-      };
 
-    default:
-      return state;
-  }
-}
-
-export default catsReducer;
+export default catsSlice.reducer;
+export const { catAdded, catUpdated } = catsSlice.actions;
